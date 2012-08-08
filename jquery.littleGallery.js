83b3ae0ff523b920995edd879a8a7c1c7af9592a
Aleksej Martirosyan <Alex-Martirosyan@ya.ru>
2012-08-08 12:30:01 +0400

    add 'Image not found'

--- jquery.littleGallery.js
+++ jquery.littleGallery.js
@@ -13,18 +13,18 @@
             });
 
             $('.littleGallery-blind').show();
-            resize();   
-            _this.find('.littleGallery-head .littleGallery-img:eq(0)').click();                
+            resize();
+            _this.find('.littleGallery-head .littleGallery-img:eq(0)').click();
         }
     });
 
     options = $.extend({
       data: {},
       marginWindow: {top: 50, left: 50},
-      paddingImage: {top: 0, left: 0},    
+      paddingImage: {top: 0, left: 0},
       circularShift: true,
       loadingImageSrc: 'loading.gif',
-      closeImageSrc: 'x.png'      
+      closeImageSrc: 'x.png'
     }, options);
 
     var arrayImages = [];
@@ -42,7 +42,7 @@
         }
 
         if (_this.innerWidth() > wW - options.marginWindow.left) {
-            _this.css('max-width', wW - options.marginWindow.left);           
+            _this.css('max-width', wW - options.marginWindow.left);
         }
 
         $('.littleGallery .littleGallery-body').height(_this.innerHeight() - heightElements);
@@ -58,7 +58,7 @@
 
         var top = parseInt(wH / 2 - eH / 2);
         var left = parseInt(wW / 2 - eW / 2);
-        _this.offset({left: left, top: top}); 
+        _this.offset({left: left, top: top});
 
         setTimeout(function () {
            if (flagResize) {
@@ -76,11 +76,11 @@
         + '<div class="littleGallery-head"></div>'
         + '<div class="littleGallery-body">   '
         + '    <div class="littleGallery-container"></div>'
-        + '</div>'     
+        + '</div>'
         +'</div> <div class="littleGallery-blind></div>' );
 
       $('body').append('<div class="littleGallery-blind"></div>');
-      _this = $('.littleGallery');    
+      _this = $('.littleGallery');
 
       var headHtml = '<span class="littleGallery-img" data="'+data.front.url+'">'+data.front.name+'</span>';
       if (data.back) {
@@ -88,23 +88,23 @@
       }
       if (data.uv_front) {
           headHtml += '<span class="littleGallery-img" data="'+data.uv_front.url+'">'+data.uv_front.name+'</span>';
-      } 
+      }
       if (data.uv_back) {
           headHtml += '<span class="littleGallery-img" data="'+data.uv_back.url+'">'+data.uv_back.name+'</span>';
-      }        
-      _this.find('.littleGallery-head').html(headHtml);     
+      }
+      _this.find('.littleGallery-head').html(headHtml);
 
       $(window).on('resize', function () {
-          resize();    
+          resize();
       });
 
       $(window).on('click', function (e) {
-          if (_this.is(':visible')) { 
+          if (_this.is(':visible')) {
               if (hideOnClick && $(e.target).closest(_this.get(0)).length == 0) {
                   _this.find('.littleGallery-img-close').click();
               }
           }
-      });      
+      });
 
       $(window).on('keyup', function (e) {
           if (!$('.littleGallery').is(':visible')) return;
@@ -131,12 +131,12 @@
 
                       return false;
                   }
-              });            
+              });
           }  else if (e.keyCode == 37) {
 
               if (el.length == 1) {
                   return false;
-              }             
+              }
 
               el.each(function () {
                   if ($(this).hasClass('littleGallery-img-activ')) {
@@ -150,17 +150,17 @@
 
                       return false;
                   }
-              });             
+              });
           }
-      });   
+      });
 
       _this.find('.littleGallery-img-close').on('click', function () {
           _this.fadeOut();
           $('.littleGallery-blind').fadeOut();
           hideOnClick = false;
-      });   
+      });
 
-      _this.find('.littleGallery-head .littleGallery-img').live('click', function () { 
+      _this.find('.littleGallery-head .littleGallery-img').live('click', function () {
           var urlImg = $(this).attr('data');
           if (!urlImg) return;
 
@@ -174,7 +174,7 @@
           }
 
           if (arrayImages[urlImg]) {
-              _this.find('.littleGallery-body .littleGallery-container').html('<img class="littleGallery-img" src="'+urlImg+'">').hide().fadeIn();              
+              _this.find('.littleGallery-body .littleGallery-container').html('<img class="littleGallery-img" src="'+urlImg+'">').hide().fadeIn();
               flagResize = true;
               resize();
           } else {
@@ -190,16 +190,21 @@
                   arrayImages[urlImg] = true;
 
                   if (lastImageLoading == urlImg) {
-                      _this.find('.littleGallery-body .littleGallery-container').html('<img class="littleGallery-img" src="'+urlImg+'">').hide().fadeIn(); ;
+                      _this.find('.littleGallery-body .littleGallery-container').html('<img class="littleGallery-img" src="'+urlImg+'">').hide().fadeIn();
                       flagResize = true;
-                      resize();                      
+                      resize();
                   }
-              });                
-          }   
-      });
+              });
 
+               $(tmp).on('error', function () {
+                   _this.find('.littleGallery-body .littleGallery-container').html('<i class="littleGallery-notFound">Image not found</i>').hide().fadeIn();
+                   flagResize = true;
+                   resize();
+               });
+          }
+      });
     };
 
-    return this.each(init); 
-  }; 
+    return this.each(init);
+  };
 })(jQuery);
